45b835e6add52812be784d983a8de6942be5b54f
Zhihan Sun <z5408739@ad.unsw.edu.au>
2026-04-18 19:00:21 +1000

    Finishing first version of parallel run
    working version that is integrated with frontend, store all runlop files in to RUNS folder

--- agent/main.py
+++ agent/main.py
@@ -40,23 +40,22 @@ def root():
 def healthcheck():
     return {"status": "healthy"}
 
-# 2. 定义跑一次模拟的“打工人”函数
 def run_single(index: int, req_dict: dict, api_keys: List[str]):
-    # 轮询分配 API Key
+    # assign API Key
     current_key = api_keys[index % len(api_keys)]
     os.environ["GROQ_API_KEY"] = current_key
     
-    # 稍微错开 seed，保证每次模拟结果不一样
-    child_seed = (req_dict['seed'] + index) if req_dict['seed'] is not None else None
-    
-    # 调用你写好的 run_simulation
+    #child_seed = (req_dict['seed'] + index) if req_dict['seed'] is not None else None
+    child_seed = req_dict['seed']
+
+    # Call run_simulation
     run_log, signal_drift = run_simulation(
         n_agents=req_dict['agent_count'],
         n_steps=req_dict['steps'],
         seed=child_seed,
         out_dir="runs",
         ground_truth=req_dict['ground_truth'],
-        run_identifier=f"run{index:02d}", # 会生成 run00, run01 文件夹
+        run_identifier=f"run{index:02d}", # generate run00, run01 files
         intra_cluster_p=req_dict['intra_cluster_p'],
         inter_cluster_m=req_dict['inter_cluster_m'],
         agents_per_cluster=req_dict['agents_per_cluster'],
@@ -68,7 +67,7 @@ def run_single(index: int, req_dict: dict, api_keys: List[str]):
 @app.post("/api/simulate")
 def simulate(req: SimulateRequest):
     try:
-        # 1. 抓取环境里的所有 API Keys
+        # 1. take all API Keys from .evn
         keys_str = os.getenv("GROQ_API_KEYS", "")
         if not keys_str:
             keys_str = os.getenv("GROQ_API_KEY", "")
@@ -81,19 +80,18 @@ def simulate(req: SimulateRequest):
 
         req_dict = req.dict()
 
-        # 2. 启动并发引擎！
+        # 2. start the parallel simulation
         with concurrent.futures.ProcessPoolExecutor(max_workers=req.simulations) as executor:
-            # 现在我们把 index, req_dict, api_keys 都作为参数传给外面的 run_single
             futures = [executor.submit(run_single, i, req_dict, api_keys) for i in range(req.simulations)]
             for future in concurrent.futures.as_completed(futures):
                 all_results.append(future.result())
 
-        # 3. 关键：为了不弄崩队友写好的前端，我们只取第一个跑完的结果返回去画图
+        # 3. construct the graph with the result that is finished first
         first_log, first_drift = all_results[0]
-        
+
         return SimulateResponse(run_log=first_log, signal_drift=first_drift)
 
     except Exception as e:
         import traceback
-        traceback.print_exc() # 在终端打印完整报错，方便你 debug
+        traceback.print_exc() # debug in the terminal
         raise HTTPException(status_code=500, detail=str(e))
